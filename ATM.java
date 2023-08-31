0be15938761bad35cc0a9c7b60cab15fbd13bc7a
Wyatt Van Amburg <wyatt.va@icloud.com>
2023-08-31 14:18:23 -0700

    Made the closeAccount method

--- ATM.java
+++ ATM.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public class ATM
 {
-    private static HashMap<String, Double> hMap = new HashMap<String, Double>();
+    private HashMap<String, Double> hMap = new HashMap<String, Double>();
     public void openAccount(String userID, Double amount) throws Exception
     {
         if (hMap.containsKey(userID))
@@ -10,5 +10,13 @@ public class ATM
             throw new Exception("User already exists");
         }
         hMap.put(userID, amount);
+    }
+    public void closeAccount(String userID) throws Exception
+    {
+        if (hMap.containsKey(userID))
+        {
+            throw new Exception("Account has a balance, please remove balance before deleting the account");
+        }
+        hMap.remove(userID);
     }
 }
